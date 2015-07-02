72a2f2fe7c88580ed27f01ea7c88a45312de015f
Binh Pham <binhpt79@gmail.com>
2015-07-02 20:43:25 +0700

    First commit

---
+++ app/src/main/java/elearning/vieted/com/fragment/CourseFragment.java
@@ -0,0 +1,26 @@
+package elearning.vieted.com.fragment;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import elearning.vieted.com.vietedelearning.R;
+
+public class CourseFragment extends Fragment {
+    public CourseFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_course, container, false);
+    }
+
+
+}
